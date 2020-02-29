939e69d3e4ac57b5d4450083cb3919c66547ced9
yafanisonya <yafanison@gmail.com>
2020-02-29 17:57:17 +0800

    添加 404 页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>
+      页面不存在，请检查网址输入是否正确
+    </div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'NotFound'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
