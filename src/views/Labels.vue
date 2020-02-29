46eba516767e3962665f7333ca66b385a1d2b675
yafanisonya <yafanison@gmail.com>
2020-02-29 20:21:08 +0800

    重复三次布局

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
-    Labels.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      Labels.vue
+    </div>
     <Nav/>
   </div>
 </template>
-
 <script lang="ts">
   export default {
     name: 'Labels',
@@ -12,5 +13,14 @@
 </script>
 
 <style lang="scss" scoped>
-
+  .nav-wrapper{
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .content{
+    overflow: auto;
+    flex-grow: 1;
+  }
 </style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
-    Statistics.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      Statistics.vue
+    </div>
     <Nav/>
   </div>
 </template>
-
 <script lang="ts">
   export default {
     name: 'Statistics',
@@ -12,5 +13,14 @@
 </script>
 
 <style lang="scss" scoped>
-
+  .nav-wrapper{
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .content{
+    overflow: auto;
+    flex-grow: 1;
+  }
 </style>
