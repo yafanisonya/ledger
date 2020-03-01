69bd7ec1ddf1258df1f59be283d7c4fe8d77e2f7
yafanisonya <yafanison@gmail.com>
2020-03-01 11:08:22 +0800

    添加 <Nav/> 样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,33 +1,45 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">
+  <nav>
+    <router-link to="/money" class="item">
       <Icon name="money"/>
       记账
     </router-link>
-    |
-    <router-link to="/labels">
+    <router-link to="/labels" class="item">
       <Icon name="labels"/>
       标签
     </router-link>
-    |
-    <router-link to="/statistics">
+    <router-link to="/statistics" class="item">
       <Icon name="statistics"/>
       统计
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
   // import x from "@/assets/icons/label.svg";
   // console.log(x);
-
   export default {
     name: 'Nav'
   };
 </script>
 
 <style lang="scss" scoped>
-  .nav{
-    border:1px solid red;
+  nav{
+    display: flex;
+    flex-direction: row;
+    box-shadow: 0 0 3px rgba(0,0,0,0.25);
+    font-size: 12px;
+    > .item{
+      padding:2px 0;
+      width:33.3333%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .icon{
+        width: 32px;
+        height:32px;
+      }
+    }
   }
 </style>
