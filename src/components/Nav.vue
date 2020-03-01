8a130619eebdc2e8fb5d1fde5f92bf2e6d77f944
yafanisonya <yafanison@gmail.com>
2020-03-01 11:53:47 +0800

    路由激活

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
-    <router-link to="/labels" class="item">
-      <Icon name="labels"/>
+    <router-link to="/labels" class="item" active-class="selected">
+      <Icon name="label"/>
       标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -22,6 +22,7 @@
     name: 'Nav'
   };
 </script>
+
 
 <style lang="scss" scoped>
   nav{
@@ -40,6 +41,9 @@
         width: 32px;
         height:32px;
       }
+    }
+    >.item.selected{
+      color: red;
     }
   }
 </style>
