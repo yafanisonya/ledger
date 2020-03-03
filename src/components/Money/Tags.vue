29ea866b5d1bef25a93ab0a1f55dd8a95f366e0b
yafanisonya <yafanison@gmail.com>
2020-03-03 14:45:11 +0800

    Money组件模块化

---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="tags">
+    <div class="new">
+      <button>新增标签</button>
+    </div>
+    <ul class="current">
+      <li>餐饮</li>
+      <li>交通</li>
+      <li>购物</li>
+      <li>娱乐</li>
+
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Tags'
+  };
+</script>
+
+<style lang="scss" scoped>
+  .tags{
+    font-size: 14px;
+    padding: 16px;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column-reverse;
+    > .current{
+      display: flex;
+      flex-wrap: wrap;
+      >li{
+        background-color: #d9d9d9;
+        $h:24px;
+        height: $h;
+        line-height: $h;
+        border-radius: $h/2;
+        padding: 0 16px ;
+        margin-right: 12px;
+        margin-top: 4px;
+      }
+    }
+    > .new{
+      padding-top:16px;
+      button{
+        background: transparent;
+        border: none;
+        color: #999;
+        border-bottom:1px solid;
+        padding:0 3px;
+      };
+    }
+  }
+</style>
