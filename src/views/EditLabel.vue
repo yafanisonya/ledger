2ea6d089168cec065a225535ecdeec061ab97454
yafanisonya <yafanison@gmail.com>
2020-03-07 08:32:48 +0800

    将tag改为{id,name}

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,16 @@
+<template>
+  <layout>编辑标签</layout>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class EditLabel extends Vue {
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
