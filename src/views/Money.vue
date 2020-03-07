78c2c697dadf3c96615e0cd4be1692ae22626705
yafanisonya <yafanison@gmail.com>
2020-03-07 11:07:41 +0800

    Notes.vue 成为通用组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,7 @@
   <layout class-prefix="layout">
     <NumberPad :value="record.amount" @update:value="onUpdateAmount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
-    <Notes @update:value="onUpdateNotes"/>
+    <Notes field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
     </layout>
 </template>
