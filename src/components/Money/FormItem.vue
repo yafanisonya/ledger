135766a170ff38cfe05f55a70b02e45793e34646
yafanisonya <yafanison@gmail.com>
2020-03-07 16:20:33 +0800

    修复value被赋值及Tags警告问题

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -2,7 +2,10 @@
   <div>
     <label class="formItem">
       <span class="name">{{this.fieldName}}</span>
-      <input type="text" v-model="value" :placeholder="this.placeholder">
+      <input type="text"
+             :value="value"
+             @input="onValueChanged($event.target.value)"
+             :placeholder="this.placeholder">
     </label>
   </div>
 </template>
@@ -12,7 +15,7 @@
  import {Component, Prop, Watch} from 'vue-property-decorator';
  @Component
  export default class FormItem extends Vue{
-   value = '';
+   @Prop({default:''}) readonly value!: string;
    @Prop({required:true}) fieldName!: string;
    @Prop() placeholder?: string;
 
