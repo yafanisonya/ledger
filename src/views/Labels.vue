eb7832b1bf7997add8234ee54ee549536600e066
yafanisonya <yafanison@gmail.com>
2020-03-08 11:33:05 +0800

    封装 tagListModel

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -17,7 +17,7 @@
 
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
-  import tagListModel from '@/models/tagListModel';
+  //import tagListModel from '@/models/tagListModel';
   import Button from '@/components/Money/Button.vue';
 
   //tagListModel.fetch();
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -18,7 +18,7 @@
   import Tags from '@/components/Money/Tags.vue';
   import {Component, Watch} from 'vue-property-decorator';
   import recordListModel from '@/models/recordListModel'
-  import tagListModel from '@/models/tagListModel';
+  //import tagListModel from '@/models/tagListModel';
   //const model = require('@/model.js').default;
   const recordList = recordListModel.fetch();
   //const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
