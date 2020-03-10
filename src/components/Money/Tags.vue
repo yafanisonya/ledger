5dc3001ba757ebbd71be08cbd5b9d883ebe6f16c
yafanisonya <yafanison@gmail.com>
2020-03-10 15:33:48 +0800

    Tags.vue 使用vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -15,12 +15,20 @@
 <script lang="ts">
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
-  import store from '@/store/index2';
 
-  @Component
+  @Component({
+    computed:{
+      tagList(){
+        return this.$store.state.tagList;
+      }
+    }
+  })
   export default class Tags extends Vue{
-    tagList = store.fetchTags();
     selectedTags: string[] = [];
+
+    created(){
+      this.$store.commit('fetchTags');
+    }
 
   toggle(tag: string){
     const index = this.selectedTags.indexOf(tag);
@@ -35,7 +43,7 @@
   create(){
     const name = window.prompt('请输入标签名');
     if (!name){ return window.alert('标签名不能为空');}
-    store.createTag(name);
+    this.$store.commit('createTag',name);
   }
 }
 </script>
