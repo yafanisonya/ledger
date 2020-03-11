ebe29cc5701b3933e9b4c8ef4b01301ec877d847
yafanisonya <yafanison@gmail.com>
2020-03-11 12:00:39 +0800

    Money.vue 使用 Tabs

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <layout class-prefix="layout">
     <NumberPad :value="record.amount" @update:value="onUpdateAmount" @submit="saveRecord"/>
-    <Types :value.sync="record.type"/>
+    <Tabs :data-source="recordTypeList" :value.sync="record.type"></Tabs>
     <div class="notes">
       <FormItem field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes"/>
     </div>
@@ -12,18 +12,22 @@
 <script lang="ts">
   import Vue from 'vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
-  import Types from '@/components/Money/Types.vue';
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {Component} from 'vue-property-decorator';
+  import Tabs from '@/components/Tabs.vue';
+  import recordTypeList from '@/constants/recordTypeList';
 
   @Component({
-    components:{NumberPad,Types,FormItem,Tags},
+    components:{Tabs, NumberPad,FormItem,Tags},
   })
   export default class Money extends Vue{
     get recordList(){
       return this.$store.state.recordList;
     }
+
+    recordTypeList = recordTypeList;
+
     record: RecordItem = {
       tags:[], notes:'', type:'-', amount:0
     };
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -13,6 +13,8 @@
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
   import Tabs from '@/components/Tabs.vue';
+  import intervalList from '@/constants/intervalList';
+  import recordTypeList from '@/constants/recordTypeList';
 
   @Component({
     components:{Tabs},
@@ -20,15 +22,8 @@
   export default class Statistics extends Vue{
     type = '-';
     interval = 'day';
-    intervalList = [
-      {text:'按天',value:'day'},
-      {text:'按周',value: 'week'},
-      {text:'按月',value:'month'},
-    ];
-    typeList = [
-      {text:'支出',value:'-'},
-      {text:'收入',value: '+'},
-    ]
+    intervalList = intervalList;
+    typeList = recordTypeList;
   }
 </script>
 
